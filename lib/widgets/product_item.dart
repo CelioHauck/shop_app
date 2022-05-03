1659c798ced5b407ba01157b9deffb6496432bc3
CelioHauck <celiohauck@gmail.com>
2022-05-03 14:49:40 -0300

    Aplicando FadeInImage nos produtos

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -48,8 +48,13 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: const AssetImage(
+              'assets/images/product-placeholder.png',
+            ),
+            image: NetworkImage(
+              product.imageUrl,
+            ),
             fit: BoxFit.cover,
           ),
         ),
